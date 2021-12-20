e4fae96186bb520af0318da15f7430c14a358672
maksim19-06-1 <78656680+maksim19-06-1@users.noreply.github.com>
2021-12-21 00:04:30 +0200

    Delete КР directory

--- КР/lect1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='index'),
-    path('hello/', views.hello, name='hello page'),
-    path('hello/<str:name>', views.hello_name, name='name'),
-]
--- КР/sessionUniversity/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Exam, Student, Subject
-admin.site.register(Exam)
-admin.site.register(Student)
-admin.site.register(Subject)
-# Register your models here.
